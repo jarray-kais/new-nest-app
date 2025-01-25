1d88635db8e2f04311b1844dbb793329b58700a6
jarray-kais <jarraykais1@gmail.com>
2025-01-25 21:19:04 +0100

    add routes get and update and delete
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateProductDto } from './dtos/create-product.dto';
+import { UpdateProductDto } from './dtos/update-product.tdo';
 
 type ProducType = { id: number; title: string; price: number };
 
@@ -25,5 +35,33 @@ export class ProductsController {
   @Get()
   public getAllProducts() {
     return this.products;
+  }
+
+  //Get : /api/products/:id
+  @Get('/:id')
+  public getSingleProducts(@Param('id') id: string) {
+    const product = this.products.find((p) => p.id === parseInt(id));
+    if (!product) throw new NotFoundException('product not found');
+    return product;
+  }
+
+  //PUT : /api/products/:id
+  @Put('/:id')
+  public updateProduct(
+    @Param('id') id: string,
+    @Body() body: UpdateProductDto,
+  ) {
+    const product = this.products.find((p) => p.id === parseInt(id));
+    if (!product) throw new NotFoundException('product not found');
+    console.log(body);
+    return { message: 'Product updated successfully' };
+  }
+
+  //delete : /api/products/:id
+  @Delete('/:id')
+  public deleteProduct(@Param('id') id: string) {
+    const product = this.products.find((p) => p.id === parseInt(id));
+    if (!product) throw new NotFoundException('product not found');
+    return { message: 'Product deleted successfully' };
   }
 }
