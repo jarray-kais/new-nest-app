d65ef08e2801bddd5964b587eeea0ddd30eb1b34
jarray-kais <jarraykais1@gmail.com>
2025-01-26 18:57:17 +0100

    create users modules
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  public getAll() {
+    return [
+      { id: 1, name: 'John Doe' },
+      { id: 2, name: 'Jane Smith' },
+      { id: 3, name: 'Alice Johnson' },
+    ];
   }
 }
