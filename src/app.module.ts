e7b2f53a6b060c59515a40ea874728b5e4a725e7
jarray-kais <jarraykais1@gmail.com>
2025-01-27 21:55:36 +0100

    import the TypeOrmModule into the root AppModule
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,23 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { ReviewsModule } from './reviews/reviews.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './products/product.entity';
 @Module({
-  imports: [UsersModule, ProductsModule, ReviewsModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    ReviewsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'nestjs-app-db',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      entities: [Product],
+      synchronize: true, //only in development
+    }),
+  ],
 })
 export class AppModule {}
