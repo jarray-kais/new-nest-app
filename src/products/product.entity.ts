b76c1c89d8b3a90b438a471e35b0a1b5f0c29bf3
jarray-kais <jarraykais1@gmail.com>
2025-01-27 21:56:19 +0100

    define the Product entity
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

---
+++ src/products/product.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+const CURRENT_TIMESTAMP = 'CURRENT_TIMESTAMP(6)';
+@Entity({ name: 'products' })
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: '150' })
+  title: string;
+  @Column({ type: 'float' })
+  price: number;
+  @Column()
+  description: string;
+  @CreateDateColumn({ type: 'timestamp', default: () => CURRENT_TIMESTAMP })
+  createdAt: Date;
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => CURRENT_TIMESTAMP,
+    onUpdate: CURRENT_TIMESTAMP,
+  })
+  updatedAt: Date;
+}
